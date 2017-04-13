a46b8a8a00204e63acd3fc3cb36ef71e2379823f
Dramiel Leayal <whitered12316@gmail.com>
2017-04-13 22:02:28 +0700

    publish a web ... ???

--- docs/myscript.js
+++ docs/myscript.js
@@ -1,4 +1,8 @@
 function GetLatestGithubReleaseInfo() {
+    $("#progressRing").show();
+    $("#downloadcontent").hide();
+    $("#downloadButton").attr("href", "");
+    SetLoading($("#progressRing"));
     $.ajax({
         cache: false,
         url: "https://api.github.com/repos/Leayal/PSO2LauncherNew/releases",
@@ -20,15 +24,51 @@ function GetLatestGithubReleaseInfo() {
             } else {
                 timeAgo = (dateDiff / oneMonth).toFixed(1) + " months ago";
             }
-            var releaseInfo = release.name + " was updated " + timeAgo;
-            $("#fafafaf").attr("href", asset.browser_download_url);
-            $("#fafafaf").text(releaseInfo);
-            $("#fafafaf").fadeIn("slow");
+            $("#downloadButton").attr("href", asset.browser_download_url);
+            $("#downloadVersion").text(release.tag_name);
+            $("#downloadLastUpload").text(timeAgo);
+            $("#downloadButton").fadeIn("slow");
+
+            for (i = 1; i < json.length; i++) {
+                release = json[i];
+                if (release && release.assets && release.assets.length > 0) {
+                    var theLi = $("<li>");
+                    theLi.append($("<a>").attr("href", release.assets[0].browser_download_url).text(release.tag_name));
+                    theLi.appendTo($("#olderversionlisting"));
+                }
+            }
+            $("#progressRing").hide();
+            $("#downloadcontent").show();
+            RemoveLoading($("#progressRing"));
         }
     });
-}
+};
 
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
-$(function() {
-    GetLatestGithubReleaseInfo();
-});
+function SetLoading(target) {
+    var found = target.find($("div[metroloading]"));
+    if (found && found.length > 0) return;
+    //target.append($("<div metroloading class=\"midcenter\"><div class=\"windows8-loading\"><b></b><b></b><b></b><b></b><b></b></div></div>"));
+    var aaasdwaf = $("<div>").addClass("stretch").addClass("windows8-loading");
+    aaasdwaf.append($("<b>"));
+    aaasdwaf.append($("<b>"));
+    aaasdwaf.append($("<b>"));
+    aaasdwaf.append($("<b>"));
+    aaasdwaf.append($("<b>"));
+    var ddd = $("<div metroloading>").addClass("midcenter").append(aaasdwaf);
+    ($("<div metroloading>")
+        .addClass("fixedDiv")
+        .addClass("stretch")
+        .addClass("disabled")
+        .addClass("opacity50")).prependTo(target);
+    target.append(ddd);
+};
+
+function RemoveLoading(target) {
+    target.children("div[metroloading]").remove();
+};
